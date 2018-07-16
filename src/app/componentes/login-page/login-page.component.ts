3e6b0e51a45e4af08a8453892c9bef9c130a84c6
Jorge Zarate <jorgezarate@gmail.com>
2018-07-16 18:56:01 -0300

    Mostrar mensajes de Ingreso y error

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService } from '../../servicios/auth.service';
 import {Router } from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login-page',
@@ -15,6 +16,7 @@ export class LoginPageComponent implements OnInit {
   constructor(
     public authService: AuthService,
     public router: Router,
+    public flashMensaje: FlashMessagesService
   ) { }
 
   ngOnInit() {
@@ -23,12 +25,12 @@ export class LoginPageComponent implements OnInit {
   onSubmitLogin() {
     this.authService.loginEmail(this.email, this.password)
     .then( (res) => {
-      /*this.flashMensaje.show('Usuario logado correctamente.',
-      {cssClass: 'alert-success', timeout: 4000});*/
+      this.flashMensaje.show('Usuario logueado correctamente.',
+      {cssClass: 'alert-success', timeout: 4000});
       this.router.navigate(['/privado']);
     }).catch((err) => {
-    /*  this.flashMensaje.show(err.message,
-      {cssClass: 'alert-danger', timeout: 4000});*/
+      this.flashMensaje.show(err.message,
+      {cssClass: 'alert-danger', timeout: 4000});
       this.router.navigate(['/login']);
     });
   }
--- src/app/componentes/register-page/register-page.component.ts
+++ src/app/componentes/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../servicios/auth.service';
 import {Router } from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register-page',
@@ -15,6 +16,7 @@ export class RegisterPageComponent implements OnInit {
   constructor(
     public authService: AuthService,
     public router: Router,
+    public flashMensaje: FlashMessagesService
   ) { }
 
   ngOnInit() {
@@ -23,13 +25,13 @@ export class RegisterPageComponent implements OnInit {
   onSubmitAddUser() {
     this.authService.registerUser(this.email, this.password)
     .then((res) => {
-    /*  this.flashMensaje.show('Usuario creado correctamente.',
-      {cssClass: 'alert-success', timeout: 4000});*/
+      this.flashMensaje.show('Usuario creado correctamente.',
+      {cssClass: 'alert-success', timeout: 4000});
      this.router.navigate(['/privado']);
     }).catch( (err) => {
       console.log(err);
-    /*  this.flashMensaje.show(err.message,
-      {cssClass: 'alert-danger', timeout: 4000});*/
+      this.flashMensaje.show(err.message,
+      {cssClass: 'alert-danger', timeout: 4000});
     });
   }
 
