74daa66cc7beb0b8e304c676b15c47a55dfd686e
Jorge Zarate <jorgezarate@gmail.com>
2018-07-16 17:50:11 -0300

    Añoadimoslogin logout

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService } from '../../servicios/auth.service';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  public email: string;
+  public password: string;
+
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin() {
+    this.authService.loginEmail(this.email, this.password)
+    .then( (res) => {
+      /*this.flashMensaje.show('Usuario logado correctamente.',
+      {cssClass: 'alert-success', timeout: 4000});*/
+      this.router.navigate(['/privado']);
+    }).catch((err) => {
+    /*  this.flashMensaje.show(err.message,
+      {cssClass: 'alert-danger', timeout: 4000});*/
+      this.router.navigate(['/login']);
+    });
   }
 
 }
